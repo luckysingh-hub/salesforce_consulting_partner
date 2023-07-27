611bfdf70030e53cde57ab2cd3a2e374e21586ca
Ashwani <ashwani.kumar@codmsoftware.com>
2023-07-27 12:02:40 +0530

    all product

---
+++ salesforce_consulting_partner/flow-automation.php
@@ -0,0 +1,137 @@
+<!-- header include  -->
+<?php include "../client/app/header.php"; ?>
+<!-- header include ENd -->
+<!--============= body container =========== -->
+<section style="background-image: url(../client//imageFoleder/background1.jpg);" id="banner">
+    <h3 class="line-1 anim-typewriter text-uppercase text-white text-center">Streamline CLM with Easy Contract Lifecycle Management Solutions</h3>
+</section>
+<section class="mt-5">
+    <div class="container">
+        <div class="section-heading">
+            We are
+            <span>Partner of TITAN</span>
+            <div class="section-line"></div>
+        </div>
+        <div class="pb-3 text-center aboutContant">In their dynamic collaboration, CODM and TITAN combine unmatched expertise in their respective fields. CODM Software, a leading innovator of technology solutions, joins forces with TITAN, known for its innovative products and services.</div>
+        <div class="pb-3 text-center aboutContant">Our partnership embodies a shared commitment to excellence, innovation, and customer satisfaction. Together, we are committed to redefining industry standards and revolutionizing the market.
+        With their wide-ranging offerings, CODM and TITAN address a variety of industries and ensure that companies of all sizes find solutions tailored to their needs</div>
+    </div>
+</section>
+
+<section class="F-T-margin-top">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 mx-auto align-self-center order-md-2 ps-md-5 mb-2">
+                <div class="T-form-tital">Achieve ground breaking workflow automation for your Salesforce orgs</div>
+                <div class="T-form-subTital pt-4">Fluently automate workflows across Salesforce orgs with zero code to streamline your business processes and enhance productivity</div>
+                <div class="mt-4"><span><i class="fa-solid fa-check T-bg-color"></i></span> <span class="fw-bold">Build</span></div>
+                <div class="mt-3"><span><i class="fa-solid fa-check T-bg-color"></i></span> <span class="fw-bold">Integrate</span></div>
+                <div class="mt-3"><span><i class="fa-solid fa-check T-bg-color"></i></span> <span class="fw-bold">Connect</span></div>
+            </div>
+            <div class="col-lg-5 mx-auto align-self-center mb-2">
+                <img src="../client//imageFoleder//sing1.png" alt="form-2" class="w-100">
+            </div>
+        </div>
+    </div>
+</section>
+<section class="F-T-margin-top">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-lg-4 mx-auto mb-3">
+                <img src="../client//imageFoleder//icon-1-1-1.svg" alt="">
+                <div class="T-F-no_cod_tital pt-2">No-code Design</div>
+                <div class="pt-3 T-F-no_cod_subtital">Build adept business processes with zero code using Titan Flow.</div>
+            </div>
+            <div class="col-lg-4 mx-auto mb-3">
+                <img src="../client//imageFoleder//icon-2-1-1.svg" alt="">
+                <div class="T-F-no_cod_tital pt-3">Includes 3rd Party Integrations</div>
+                <div class="pt-3 T-F-no_cod_subtital">Connect payment providers, cloud based storage, and more</div>
+            </div>
+            <div class="col-lg-4 mx-auto mb-3">
+                <img src="../client//imageFoleder//icon-4-2.svg" alt="">
+                <div class="T-F-no_cod_tital pt-2">End-to-end Secure</div>
+                <div class="pt-3 T-F-no_cod_subtital">Secure sensitive organizational data with end-to-end encryption</div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="F-T-margin-top">
+    <div class="container">
+        <div class="row ">
+            <div class="col-lg-7 mx-auto align-self-center  p-md-5">
+                <div class="T-form-tital align-self-center">Create systematic and connected approval processes</div>
+                <div class="T-form-subTital pt-4">Build agile approval processes, which connect users both in and outside of Salesforce</div>
+            </div>
+                    <div class="col-lg-5 mx-auto">
+                <img src="../client//imageFoleder//Easilygeneratecustom.png" alt="form-1" class="w-100">
+            </div>
+           
+        </div>
+    </div>
+</section>
+
+<section class="F-T-margin-top">
+    <div class="container">
+        <div class="row ">
+            <div class="col-lg-7 mx-auto align-self-center  p-md-5">
+                <div class="T-form-tital align-self-center">Push and pull real-time Salesforce data</div>
+                <div class="T-form-subTital pt-4">Create integrated workflows with the capability of pushing and pulling real-time data directly from your team’s Salesforce databases</div>
+            </div>
+                    <div class="col-lg-5 mx-auto">
+                <img src="../client//imageFoleder//Easilygeneratecustom.png" alt="form-1" class="w-100">
+            </div>
+           
+        </div>
+    </div>
+</section>
+
+<section class="F-T-margin-top">
+    <div class="container">
+        <div class="row ">
+            <div class="col-lg-7 mx-auto align-self-center  p-md-5">
+                <div class="T-form-tital align-self-center">Automate repetitive tasks and templates</div>
+                <div class="T-form-subTital pt-4">Effortlessly create digital workflows connecting customers, partners, and employees in order to do more in less time</div>
+            </div>
+                    <div class="col-lg-5 mx-auto">
+                <img src="../client//imageFoleder//Easilygeneratecustom.png" alt="form-1" class="w-100">
+            </div>
+           
+        </div>
+    </div>
+</section>
+
+
+<section class="mt-5">
+    <div class="container">
+        <div class="section-heading">
+            Titan Flow   
+            <span>Features</span>
+            <div class="section-line"></div>
+        </div>
+        <div class="row pt-4">
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Conditional Logic</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Payment Integrations</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Maps & Schedulers</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Responsive Design</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Dynamic Content</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Advanced Reports</div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- footer include  -->
+<?php include "../client/app/footer.php"; ?>
+<!-- footer include ENd -->
---
+++ salesforce_consulting_partner/web-application.php
@@ -0,0 +1,137 @@
+<!-- header include  -->
+<?php include "../client/app/header.php"; ?>
+<!-- header include ENd -->
+<!--============= body container =========== -->
+<section style="background-image: url(../client//imageFoleder/background1.jpg);" id="banner">
+    <h3 class="line-1 anim-typewriter text-uppercase text-white text-center">Streamline CLM with Easy Contract Lifecycle Management Solutions</h3>
+</section>
+<section class="mt-5">
+    <div class="container">
+        <div class="section-heading">
+            We are
+            <span>Partner of TITAN</span>
+            <div class="section-line"></div>
+        </div>
+        <div class="pb-3 text-center aboutContant">In their dynamic collaboration, CODM and TITAN combine unmatched expertise in their respective fields. CODM Software, a leading innovator of technology solutions, joins forces with TITAN, known for its innovative products and services.</div>
+        <div class="pb-3 text-center aboutContant">Our partnership embodies a shared commitment to excellence, innovation, and customer satisfaction. Together, we are committed to redefining industry standards and revolutionizing the market.
+        With their wide-ranging offerings, CODM and TITAN address a variety of industries and ensure that companies of all sizes find solutions tailored to their needs</div>
+    </div>
+</section>
+
+<section class="F-T-margin-top">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 mx-auto align-self-center order-md-2 p-md-5 mb-2">
+                <div class="T-form-tital">A dynamic web application builder for Salesforce teams</div>
+                <div class="T-form-subTital pt-4">Easily design, deploy, and measure custom web applications directly from Salesforce using Titan’s drag-and-drop web page builder.</div>
+                <div class="mt-4"><span><i class="fa-solid fa-check T-bg-color"></i></span> <span class="fw-bold">Configure</span></div>
+                <div class="mt-3"><span><i class="fa-solid fa-check T-bg-color"></i></span> <span class="fw-bold">Design</span></div>
+                <div class="mt-3"><span><i class="fa-solid fa-check T-bg-color"></i></span> <span class="fw-bold">Deploy</span></div>
+            </div>
+            <div class="col-lg-5 mx-auto align-self-center mb-2">
+                <img src="../client//imageFoleder//web-1.png" alt="form-2" class="w-100">
+            </div>
+        </div>
+    </div>
+</section>
+<section class="F-T-margin-top">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-lg-4 mx-auto mb-3">
+                <img src="../client//imageFoleder//web-icon-1.svg" alt="">
+                <div class="T-F-no_cod_tital pt-2">No-code Design</div>
+                <div class="pt-3 T-F-no_cod_subtital">Build custom web pages and portals with Titan’s simple drag and drop builder.</div>
+            </div>
+            <div class="col-lg-4 mx-auto mb-3">
+                <img src="../client//imageFoleder//web-icon-2.svg" alt="">
+                <div class="T-F-no_cod_tital pt-3">Regulatory Compliant</div>
+                <div class="pt-3 T-F-no_cod_subtital">Ensure ultimate compliance with regulatory frameworks, including HIPPA and GDPR.</div>
+            </div>
+            <div class="col-lg-4 mx-auto mb-3">
+                <img src="../client//imageFoleder//web-icon-3.svg" alt="">
+                <div class="T-F-no_cod_tital pt-2">End-to-end Secure</div>
+                <div class="pt-3 T-F-no_cod_subtital">Secure sensitive organizational data with SalesForce Shield’s end-to-end encryption.</div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="F-T-margin-top">
+    <div class="container">
+        <div class="row ">
+            <div class="col-lg-7 mx-auto align-self-center  p-md-5">
+                <div class="T-form-tital align-self-center">Design delightful end-user experiences</div>
+                <div class="T-form-subTital pt-4">Deploy custom web pages and digital experiences that are user-friendly and easily accessible to a wide-range of audiences.</div>
+            </div>
+                    <div class="col-lg-5 mx-auto">
+                <img src="../client//imageFoleder//web-2.png" alt="form-1" class="w-100">
+            </div>
+           
+        </div>
+    </div>
+</section>
+
+<section class="F-T-margin-top">
+    <div class="container">
+        <div class="row ">
+            <div class="col-lg-7 mx-auto align-self-center order-md-2 p-md-5 mb-2">
+                <div class="T-form-tital align-self-center">Push and pull real-time Salesforce data</div>
+                <div class="T-form-subTital pt-4">Share bi-directional documents with the ability to push and pull real-time data directly from your team’s Salesforce databases.</div>
+            </div>
+                    <div class="col-lg-5 mx-auto">
+                <img src="../client//imageFoleder//web-3.png" alt="form-1" class="w-100">
+            </div>
+           
+        </div>
+    </div>
+</section>
+
+<section class="F-T-margin-top">
+    <div class="container">
+        <div class="row ">
+            <div class="col-lg-7 mx-auto align-self-center  p-md-5">
+                <div class="T-form-tital align-self-center">Automate repetitive tasks and templates</div>
+                <div class="T-form-subTital pt-4">Easily create digital work processes between customers, partners, and employees in order to do more in less time.</div>
+            </div>
+                    <div class="col-lg-5 mx-auto">
+                <img src="../client//imageFoleder//web-4.png" alt="form-1" class="w-100">
+            </div>
+           
+        </div>
+    </div>
+</section>
+
+
+<section class="mt-5">
+    <div class="container">
+        <div class="section-heading">
+            Titan Web  
+            <span>Features</span>
+            <div class="section-line"></div>
+        </div>
+        <div class="row pt-4">
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Dynamic Content</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Responsive Design</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">CMS Integration</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">SSO Admin Portal</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Password Protection</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Maps & Schedulers</div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- footer include  -->
+<?php include "../client/app/footer.php"; ?>
+<!-- footer include ENd -->
