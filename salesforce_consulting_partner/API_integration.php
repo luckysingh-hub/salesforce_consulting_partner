8cd97840006a63a34b5ef2e8fbc75f94ee5989e3
lucky <lakusingh39@gmail.com>
2023-08-08 12:42:20 +0530

    Add page Service

---
+++ salesforce_consulting_partner/API_integration.php
@@ -0,0 +1,191 @@
+<?php 
+include "../client/app/header.php";
+?>
+
+<section class="headerAbout container-fluid">
+    <div class="row">
+        <div class="col-lg-6 aboutHeading">
+            <h2>API Integration</h2>
+
+        </div>
+        <div class="col-lg-6">
+        </div>
+    </div>
+</section>
+
+<section class="mt-5">
+    <h2 class="heading-API fontColor text-center"><i>We offers multiple kinds of APIs for the data to move through. they are</i></h2>
+</section>
+
+<section class="mt-5 container">
+    <div class="row">
+    <div class="col-lg-4" id="sidenev_API mb-2">
+            <a href="./API-Integration.php">
+                <div class="more_service_btn1 more_service_btn hoverActive">
+                    API Integration
+                </div>
+            </a>
+            <a href="./Implementation-Services.php">
+                <div class="more_service_btn2 more_service_btn">
+                    Salesforce Implementation
+                </div>
+            </a>
+            <a href="./Consulting-Services.php">
+                <div class="more_service_btn3 more_service_btn">
+                    Consulting Services
+                </div>
+            </a>
+            <a href="./Support-And-Maintenance.php">
+                <div class="more_service_btn4 more_service_btn">
+                    Support And Maintenance
+                </div>
+            </a>
+            <a href="./Data-Migration.php">
+                <div class="more_service_btn5 more_service_btn">
+                    Data Migration
+                </div>
+            </a>
+            <a href="./Testing-Deployment.php">
+                <div class="more_service_btn6 more_service_btn">
+                    Testing and Deployment
+                </div>
+            </a>
+            <div class="contact-widget mt-3">
+                <div class="widget-content" style="background-image: url(./image/pattern-25.png)">
+                    <div class="title">
+                         Work Together </div>
+                    <a href="#" class="theme-btn contact-btn">Contact Us</a>
+                    <div class="icon-flex">
+                    <i class="fa-solid fa-phone icon"></i>
+                         <i class="fa-solid fa-envelope icon"></i>
+                    </div>
+                    <div class="phone">
+                        
+                    
+                   
+                        Call :<a href="tel:+91 9871 717 425">+91 9871 717 425</a>
+                    </div>
+                    <div class="email">
+                    
+                         E-mail :<a href="mailto:hr@codmsoftware.com">hr@codmsoftware.com</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-lg-8 ">
+            <img src="./image/api.png" alt="" class="w-100 pabsluteAPI_image">
+            <h2 class="pt-2 heading_titel">API Integration</h2>
+            <p class="pt-1 heading_p">CodM offers various web service APIs such as REST, SOAP, BULK, Composite, and Apex for transactional access to create, read, edit, and delete data. The Composite resources enable interacting with related data in one API call. These APIs reduce manual data entry in multiple applications, provide support for build release management, and version control activities. Salesforce offers several API options such as Salesforce Connect, Apex callouts, External Services, MuleSoft Composer, Content Management Systems, Change Data Capture, Platform Events, Data Pipelines, OmniStudio, and AppExchange for data movement.</p>
+            <div class="container">
+                <div class="owl-carousel owl-theme text-center mt-5  " id="API-owl">
+                    <div class="item py-5 api-box-offers text-center">
+                        <span class="box-designe-line-top"></span>
+                        <span>Salesforce Connect</span>
+                    </div>
+                    <div class="item py-5 api-box-offers text-center">
+                        <span>Apex callouts</span>
+                    </div>
+                    <div class="item py-5 api-box-offers text-center">
+                        <span>External Services</span>
+                    </div>
+                    <div class="item py-5 api-box-offers text-center">
+                        <span>MuleSoft Composer</span>
+                    </div>
+                    <div class="item py-5 api-box-offers text-center">
+                        <span>Content Management</span>
+                    </div>
+                    <div class="item py-5 api-box-offers text-center">
+                        <span>Change Data Capture</span>
+                    </div>
+                    <div class="item py-5 api-box-offers text-center">
+                        <span>Platform Events</span>
+                    </div>
+                    <div class="item py-5 api-box-offers text-center">
+                        <span>Data Pipelines</span>
+                    </div>
+                    <div class="item py-5 api-box-offers text-center">
+                        <span>OmniStudio</span>
+                    </div>
+                    <div class="item py-5 api-box-offers text-center">
+                        <span>AppExchange</span>
+                    </div>
+                </div>
+            </div>
+            <h2 class="pt-5 heading_titel">Benefits of Salesforce API Integration</h2>
+            <div class="accordion mt-2 mb-3" id="accordionExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                           1. Improved Data Accuracy
+                        </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse " aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            By integrating Salesforce API with other systems, you can ensure that all data is up-to-date and accurate across all platforms. This will help to eliminate data silos and improve data quality, which will lead to better decision making and improved business processes.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            2. Increased Efficiency
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            Salesforce API integration can automate manual processes, reducing the need for manual data entry and increasing efficiency. This can help to reduce errors and improve the overall speed of business processes.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            3.Streamlined Customer Service
+                        </button>
+                    </h2>
+                    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            Salesforce API integration can provide a 360-degree view of customers, allowing you to better understand their needs and preferences. This can help to improve customer service and increase customer satisfaction.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingfour">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapsefour" aria-expanded="false" aria-controls="collapsefour">
+                           4. Better Sales Management
+                        </button>
+                    </h2>
+                    <div id="collapsefour" class="accordion-collapse collapse" aria-labelledby="headingfour" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            Salesforce API integration can help sales teams to manage leads and opportunities more effectively. It can provide real-time insights into the sales pipeline, helping to identify trends and opportunities for growth.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingfive">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapsefive" aria-expanded="false" aria-controls="collapsefive">
+                            5.Cost Savings
+                        </button>
+                    </h2>
+                    <div id="collapsefive" class="accordion-collapse collapse" aria-labelledby="headingfive" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            By automating manual processes and improving efficiency, Salesforce API integration can help to reduce costs and increase profitability. It can also help to reduce the need for additional staff, saving on recruitment and training costs.
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+        </div>
+    </div>
+</section>
+
+
+
+<!--============= body container End =========== -->
+<!-- footer include  -->
+<?php include "../client/app/footer.php"; ?>
+<!-- footer include END -->
---
+++ salesforce_consulting_partner/Implementation-Services.php
@@ -0,0 +1,158 @@
+<?php 
+include "../client/app/header.php";
+?>
+
+<section class="headerAbout container-fluid">
+    <div class="row">
+        <div class="col-lg-6 aboutHeading">
+            <!-- <h1>Services</h1> -->
+            <h2>Implementation Services</h2>
+        </div>
+        <div class="col-lg-6">
+        </div>
+    </div>
+</section>
+
+<section class="mt-5 container">
+    <div class="row">
+    <div class="col-lg-4" id="sidenev_API">
+            <a href="./API-Integration.php">
+                <div class="more_service_btn1 more_service_btn ">
+                    API Integration
+                </div>
+            </a>
+            <a href="./Implementation-Services.php">
+                <div class="more_service_btn2 more_service_btn hoverActive">
+                    Salesforce Implementation
+                </div>
+            </a>
+            <a href="./Consulting-Services.php">
+                <div class="more_service_btn3 more_service_btn">
+                    Consulting Services
+                </div>
+            </a>
+            <a href="./Support-And-Maintenance.php">
+                <div class="more_service_btn4 more_service_btn">
+                    Support And Maintenance
+                </div>
+            </a>
+            <a href="./Data-Migration.php">
+                <div class="more_service_btn5 more_service_btn">
+                    Data Migration
+                </div>
+            </a>
+            <a href="./Testing-Deployment.php">
+                <div class="more_service_btn6 more_service_btn">
+                    Testing and Deployment
+                </div>
+            </a>
+            <div class="contact-widget mt-3">
+                <div class="widget-content" style="background-image: url(./image/pattern-25.png)">
+                    <div class="title">
+                         Work Together </div>
+                    <a href="#" class="theme-btn contact-btn">Contact Us</a>
+                    <div class="icon-flex">
+                    <i class="fa-solid fa-phone icon"></i>
+                         <i class="fa-solid fa-envelope icon"></i>
+                    </div>
+                    <div class="phone">
+                        
+                    
+                   
+                        Call :<a href="tel:+91 9871 717 425">+91 9871 717 425</a>
+                    </div>
+                    <div class="email">
+                    
+                         E-mail :<a href="mailto:hr@codmsoftware.com">hr@codmsoftware.com</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-lg-8 mt-3">
+            <img src="./image/impl.png" alt="" class="w-100 pabsluteAPI_image">
+            <h2 class="pt-2 heading_titel">Implementation</h2>
+            <p class="pt-1 heading_p">CodM offers Salesforce implementation services, including consulting, configuration, customization, migration, and integration. They also provide long-term support, development, and maintenance services. They use leading Salesforce cloud solutions to help companies establish sustainable growth, operational efficiency, and an integrated customer experience. Their team of Salesforce implementation consultants is skilled at supporting businesses of all sizes, including small non-profits and large global enterprises. Thorough planning is a key element of their approach to ensure success in any Salesforce implementation project.</p>
+
+            <h2 class="pt-5 heading_titel">Salesforce implementation services can benefit businesses </h2>
+            <div class="accordion mt-2 mb-3" id="accordionExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                            Customization
+                        </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse " aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            Salesforce implementation services allow businesses to tailor the platform to their specific needs and processes, enabling them to operate more efficiently.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Improved Productivity
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            By automating workflows and streamlining business processes, Salesforce implementation services can increase productivity and efficiency, allowing employees to focus on value-added activities..
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Better Data Management
+                        </button>
+                    </h2>
+                    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            Salesforce implementation services can help businesses to manage their data more effectively, leading to better data accuracy and consistency and improved decision-making.
+
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingfour">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapsefour" aria-expanded="false" aria-controls="collapsefour">
+                            Enhanced Customer Experience
+                        </button>
+                    </h2>
+                    <div id="collapsefour" class="accordion-collapse collapse" aria-labelledby="headingfour" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            Salesforce implementation services can enable businesses to provide a better customer experience by personalizing interactions and having a 360-degree view of customer interactions.
+
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingfive">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapsefive" aria-expanded="false" aria-controls="collapsefive">
+                            Scalability </button>
+                    </h2>
+                    <div id="collapsefive" class="accordion-collapse collapse" aria-labelledby="headingfive" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            Salesforce implementation services can help businesses to scale their operations and adapt to changing requirements, ensuring they can continue to operate efficiently as they grow.
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+<!-- footer include  -->
+<?php include "../client/app/footer.php"; ?>
+<!-- footer include END -->
